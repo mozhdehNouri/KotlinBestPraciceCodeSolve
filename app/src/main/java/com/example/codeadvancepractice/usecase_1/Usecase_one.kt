5f7da3181e4bcfeed37569c2dbe0b65e3f77edc7
mozhdeh.nouri <mozhdehnouri@yahoo.com>
2024-08-14 16:24:14 +0330

    #2

--- app/src/main/java/com/example/codeadvancepractice/usecase_1/Usecase_one.kt
+++ app/src/main/java/com/example/codeadvancepractice/usecase_1/Usecase_one.kt
@@ -1,22 +1,51 @@
 package com.example.codeadvancepractice.usecase_1
 
 /*
-problem: The sum of the numbers is 12
+problem: The sum of the numbers being 12
  */
-
 
 fun main() {
     val numberList = listOf(3, 5, 9, 8, 7, 9, 1, 2)
-    val assoc = numberList.associateBy { value ->
-        println(12 - value)
-    }
-    val a = numberList.firstNotNullOf {
-        val b = assoc[it]
-        if (b != null){
-            Pair(it,b)
-        }
-        else null
+    val value = numberList.withTwoNumbers(12)
 
-    }
 
+    // Map: x->(y,z) where y+z =2020
+//    val complementPairs: Map<Int, Pair<Int, Int>?> =
+//        numberList.associateWith {
+//            numberList.withThreeNumbers(12 - it)
+//        }
+
+//    numberList.firstNotNullOfOrNull {
+//        val pair = numberList.withThreeNumbers(12 - it)
+//        if (pair != null) Triple(it, pair.first, pair.second) else null
+//    }
+
+}
+
+
+fun List<Int>.withTwoNumbers(sum: Int) {
+
+    // [3, 5, 9, 8, 7, 9, 1, 2]
+    //{9=3, 7=5, 3=9, 4=8, 5=7, 11=1, 10=2}
+    // if we print both of them the output is like above
+    // now we must check if both number exist in list its mean we found the number like 7=5
+    // we have 7 and 5 and sum is 12
+    val complements = associateBy { sum - it }
+
+    val value = map { number ->
+        val complement = complements[number]
+        if (complement != null) {
+            Pair(number, complement)
+        } else null
+    }
+}
+
+fun List<Int>.withThreeNumbers(sum: Int): Pair<Int, Int>? {
+    val complements = associateBy { sum - it }
+    return firstNotNullOfOrNull { number ->
+        val complement = complements[number]
+        if (complement != null) {
+            Pair(number, complement)
+        } else null
+    }
 }
